7bcf9680925d7f759c808eea70bee81f3ad65f5b
GrimReaper91 <120193333+GrimReaper91@users.noreply.github.com>
2023-12-04 10:57:08 +0100

    last call

---
+++ script.js
@@ -0,0 +1,52 @@
+//Uppgift 4
+
+const box = document.getElementById("divStyle");
+
+//hämta alla element med klassen 'textfield'
+const textfields = document.querySelectorAll(".textfield");
+
+// hämta en HTMLCollection av alla element med klassen button??
+
+const buttons = document.getElementsByTagName("button");
+
+//hämta första elementet med klassen empty_div
+const emptyDiv = document.querySelector(".empty_div");
+
+// uppgift 5
+function handleInputChange(e) {
+    // Skriv ut event.target till konsolen
+    console.log('Event target:', e.target);
+
+    const name = e.target.name;    
+    console.log("Input name attribute: ", name);
+
+    // Kontrollera om det element som orsakade eventet har 'name'-attributet 'content'
+    if (name=== 'content') {
+      // Skriv ut värdet i 'content'-inputfältet till det första 'div'-elementet med klassen 'empty_div'
+      emptyDiv.innerHTML = e.target.value;
+    
+    }
+   
+    } 
+    
+// Uppgift 6 
+textfields.forEach(textfield=>{
+    if(textfield.name=="color"){
+        box.addEventListener("change",function(){
+            emptyDiv.style.backgroundColor=textfield.value;
+        });
+    }
+});
+
+
+//eventlyssnare till alla textfields
+textfields.forEach((textfield) => textfield.addEventListener("change", handleInputChange));
+   
+
+
+
+buttons[0].addEventListener("click", function() {
+    emptyDiv.innerHTML = ''; // Detta tar bort allt innehåll inuti div-elementet
+    
+});
+
